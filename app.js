9db933b4ff816dc80b613180d88f99acadc3de94
aafrinabid <mohdaafrin@gmail.com>
2022-03-01 16:36:01 +0530

    lexical environment

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-let name ={
-    firstName:'Aafrin',
-    lastName:'abid',
-    
-}
-printFullName=function(hometown,state){
-    console.log(this.firstName+" "+this.lastName+' from '+hometown+','+state)
-}
-printFullName.call(name,'mumbai','maharashtra')
-
-let name2={
-    firstName:'habibi',
-    lastName:'angadi',
-
+function a (){
+    var b=10;
+    c()
+   function c(){
+        console.log(b)
+    }
 }
 
-//function borrowing
-printFullName.call(name2,'shedi angadi','kabirkasoor');
-printFullName.apply(name2,['mumbai','maharashtra']);
 
-let printName=printFullName.bind(name2,'mallu','sccenantoo')
-printName()
+a()
